fe4a3a7d4673b78223db07b8a7f9fcf0b9720f77
stephenscruggs164 <stephenscruggs164@gmail.com>
2019-03-27 10:30:44 -0500

    steve- update git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,10 @@
+
+/*
+ ** Stephen Scruggs
+ ** program name: bankApp.java
+ ** This program provides a menu of banking options.
+ */
+
 package com.company;
 import java.util.Scanner;
 
@@ -5,43 +12,10 @@ public class Main {
     public static Integer total;
 
     public static void main(String[] args) {
-        if (total == null) {
-            total = 10;
-        }
-      //  float total = 10;
-        Scanner in = new Scanner(System.in);
-        int userChoice;
-        boolean quit = false;
-        int total = 10;
-        do {
-            System.out.println("1. Deposit");
-            System.out.println("2. Withdraw");
-            System.out.println("3. Balance");
-            System.out.print("Your choice, 0 to quit: ");
-            userChoice = in.nextInt();
-            switch (userChoice) {
-                case 1:
-                    BankAccount.deposit(total);
-                    break;
-                case 2:
-                    BankAccount.withdraw();
-                      break;
-                case 3:
-                    BankAccount.balance();
-                    break;
-                case 0:
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("Wrong choice.");
-                    break;
 
-            }
-            System.out.println();
-        } while (!quit);
-            System.out.println("Visit again!");
+        BankAccount stephen = new BankAccount();
+        stephen.account();
+        stephen.showMenu();
 
-        return total;
     }
-    }
-
+}
